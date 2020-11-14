4da69a2ba2b7e99477d873ff2c64df609ddf728e
VICTOR MANUEL CASAS GUZMAN <mmhh66m@gmail.com>
2020-11-13 20:46:29 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoSandra/Form1.Designer.vb
@@ -0,0 +1,219 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.OperacionesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.InsertarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DepartamentoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClientesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HorarioToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ProvedoresToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ProductosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PedidosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SucursalesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ConsultarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ModificarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EliminarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MovimientosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PedidosToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RegistrarComprasToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ContratarEmpleadoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ReportesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EmpleadosPorAñoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.VentasPorProductosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ComprasPorProvedoresToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OperacionesToolStripMenuItem, Me.MovimientosToolStripMenuItem, Me.ReportesToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'OperacionesToolStripMenuItem
+        '
+        Me.OperacionesToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.InsertarToolStripMenuItem, Me.ConsultarToolStripMenuItem, Me.ModificarToolStripMenuItem, Me.EliminarToolStripMenuItem})
+        Me.OperacionesToolStripMenuItem.Name = "OperacionesToolStripMenuItem"
+        Me.OperacionesToolStripMenuItem.Size = New System.Drawing.Size(85, 20)
+        Me.OperacionesToolStripMenuItem.Text = "Operaciones"
+        '
+        'InsertarToolStripMenuItem
+        '
+        Me.InsertarToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.DepartamentoToolStripMenuItem, Me.ClientesToolStripMenuItem, Me.HorarioToolStripMenuItem, Me.ProvedoresToolStripMenuItem, Me.ProductosToolStripMenuItem, Me.PedidosToolStripMenuItem, Me.SucursalesToolStripMenuItem})
+        Me.InsertarToolStripMenuItem.Name = "InsertarToolStripMenuItem"
+        Me.InsertarToolStripMenuItem.Size = New System.Drawing.Size(125, 22)
+        Me.InsertarToolStripMenuItem.Text = "insertar"
+        '
+        'DepartamentoToolStripMenuItem
+        '
+        Me.DepartamentoToolStripMenuItem.Name = "DepartamentoToolStripMenuItem"
+        Me.DepartamentoToolStripMenuItem.Size = New System.Drawing.Size(150, 22)
+        Me.DepartamentoToolStripMenuItem.Text = "Departamento"
+        '
+        'ClientesToolStripMenuItem
+        '
+        Me.ClientesToolStripMenuItem.Name = "ClientesToolStripMenuItem"
+        Me.ClientesToolStripMenuItem.Size = New System.Drawing.Size(150, 22)
+        Me.ClientesToolStripMenuItem.Text = "clientes"
+        '
+        'HorarioToolStripMenuItem
+        '
+        Me.HorarioToolStripMenuItem.Name = "HorarioToolStripMenuItem"
+        Me.HorarioToolStripMenuItem.Size = New System.Drawing.Size(150, 22)
+        Me.HorarioToolStripMenuItem.Text = "horario"
+        '
+        'ProvedoresToolStripMenuItem
+        '
+        Me.ProvedoresToolStripMenuItem.Name = "ProvedoresToolStripMenuItem"
+        Me.ProvedoresToolStripMenuItem.Size = New System.Drawing.Size(150, 22)
+        Me.ProvedoresToolStripMenuItem.Text = "Provedores"
+        '
+        'ProductosToolStripMenuItem
+        '
+        Me.ProductosToolStripMenuItem.Name = "ProductosToolStripMenuItem"
+        Me.ProductosToolStripMenuItem.Size = New System.Drawing.Size(150, 22)
+        Me.ProductosToolStripMenuItem.Text = "productos"
+        '
+        'PedidosToolStripMenuItem
+        '
+        Me.PedidosToolStripMenuItem.Name = "PedidosToolStripMenuItem"
+        Me.PedidosToolStripMenuItem.Size = New System.Drawing.Size(150, 22)
+        Me.PedidosToolStripMenuItem.Text = "pedidos"
+        '
+        'SucursalesToolStripMenuItem
+        '
+        Me.SucursalesToolStripMenuItem.Name = "SucursalesToolStripMenuItem"
+        Me.SucursalesToolStripMenuItem.Size = New System.Drawing.Size(150, 22)
+        Me.SucursalesToolStripMenuItem.Text = "sucursales"
+        '
+        'ConsultarToolStripMenuItem
+        '
+        Me.ConsultarToolStripMenuItem.Name = "ConsultarToolStripMenuItem"
+        Me.ConsultarToolStripMenuItem.Size = New System.Drawing.Size(125, 22)
+        Me.ConsultarToolStripMenuItem.Text = "consultar"
+        '
+        'ModificarToolStripMenuItem
+        '
+        Me.ModificarToolStripMenuItem.Name = "ModificarToolStripMenuItem"
+        Me.ModificarToolStripMenuItem.Size = New System.Drawing.Size(125, 22)
+        Me.ModificarToolStripMenuItem.Text = "modificar"
+        '
+        'EliminarToolStripMenuItem
+        '
+        Me.EliminarToolStripMenuItem.Name = "EliminarToolStripMenuItem"
+        Me.EliminarToolStripMenuItem.Size = New System.Drawing.Size(125, 22)
+        Me.EliminarToolStripMenuItem.Text = "Eliminar"
+        '
+        'MovimientosToolStripMenuItem
+        '
+        Me.MovimientosToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PedidosToolStripMenuItem1, Me.RegistrarComprasToolStripMenuItem, Me.ContratarEmpleadoToolStripMenuItem})
+        Me.MovimientosToolStripMenuItem.Name = "MovimientosToolStripMenuItem"
+        Me.MovimientosToolStripMenuItem.Size = New System.Drawing.Size(89, 20)
+        Me.MovimientosToolStripMenuItem.Text = "movimientos"
+        '
+        'PedidosToolStripMenuItem1
+        '
+        Me.PedidosToolStripMenuItem1.Name = "PedidosToolStripMenuItem1"
+        Me.PedidosToolStripMenuItem1.Size = New System.Drawing.Size(180, 22)
+        Me.PedidosToolStripMenuItem1.Text = "registrar pedidos"
+        '
+        'RegistrarComprasToolStripMenuItem
+        '
+        Me.RegistrarComprasToolStripMenuItem.Name = "RegistrarComprasToolStripMenuItem"
+        Me.RegistrarComprasToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.RegistrarComprasToolStripMenuItem.Text = "Registrar Compras"
+        '
+        'ContratarEmpleadoToolStripMenuItem
+        '
+        Me.ContratarEmpleadoToolStripMenuItem.Name = "ContratarEmpleadoToolStripMenuItem"
+        Me.ContratarEmpleadoToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ContratarEmpleadoToolStripMenuItem.Text = "Contratar Empleado"
+        '
+        'ReportesToolStripMenuItem
+        '
+        Me.ReportesToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.EmpleadosPorAñoToolStripMenuItem, Me.VentasPorProductosToolStripMenuItem, Me.ComprasPorProvedoresToolStripMenuItem})
+        Me.ReportesToolStripMenuItem.Name = "ReportesToolStripMenuItem"
+        Me.ReportesToolStripMenuItem.Size = New System.Drawing.Size(65, 20)
+        Me.ReportesToolStripMenuItem.Text = "Reportes"
+        '
+        'EmpleadosPorAñoToolStripMenuItem
+        '
+        Me.EmpleadosPorAñoToolStripMenuItem.Name = "EmpleadosPorAñoToolStripMenuItem"
+        Me.EmpleadosPorAñoToolStripMenuItem.Size = New System.Drawing.Size(205, 22)
+        Me.EmpleadosPorAñoToolStripMenuItem.Text = "Empleados por año"
+        '
+        'VentasPorProductosToolStripMenuItem
+        '
+        Me.VentasPorProductosToolStripMenuItem.Name = "VentasPorProductosToolStripMenuItem"
+        Me.VentasPorProductosToolStripMenuItem.Size = New System.Drawing.Size(205, 22)
+        Me.VentasPorProductosToolStripMenuItem.Text = "Ventas por Productos"
+        '
+        'ComprasPorProvedoresToolStripMenuItem
+        '
+        Me.ComprasPorProvedoresToolStripMenuItem.Name = "ComprasPorProvedoresToolStripMenuItem"
+        Me.ComprasPorProvedoresToolStripMenuItem.Size = New System.Drawing.Size(205, 22)
+        Me.ComprasPorProvedoresToolStripMenuItem.Text = "Compras por provedores"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents OperacionesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents InsertarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DepartamentoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClientesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HorarioToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ProvedoresToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ProductosToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PedidosToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SucursalesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ConsultarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ModificarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EliminarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents MovimientosToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PedidosToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents RegistrarComprasToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ContratarEmpleadoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ReportesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EmpleadosPorAñoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents VentasPorProductosToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ComprasPorProvedoresToolStripMenuItem As ToolStripMenuItem
+End Class
