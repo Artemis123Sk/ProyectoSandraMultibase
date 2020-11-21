e6a6676085f13e4ef631bf3012f28e795a78c9ed
Jessica Jazmin Gonzalez Romero <jg9866445@gmail.com>
2020-11-20 18:34:54 -0600

    Consulta Empleados
    terminada, es una general de empleados

--- ProyectoSandra/frmConsultaEmpleados.vb
+++ ProyectoSandra/frmConsultaEmpleados.vb
@@ -7,8 +7,9 @@ Public Class frmConsultaEmpleados
         Try
             Myconnection.ConnectionString = "Server=localhost;Port=5432; Database=lala;UserId=postgres;Password=bts"
             Myconnection.Open()
+
             If Myconnection.State = ConnectionState.Open Then
-                MessageBox.Show("conectado")
+                'MessageBox.Show("conectado")
             Else
                 MessageBox.Show("Error de coneccion")
             End If
@@ -20,6 +21,8 @@ Public Class frmConsultaEmpleados
 
         Dim command As Npgsql.NpgsqlCommand = New Npgsql.NpgsqlCommand("select * from Empleados;", Myconnection)
         command.ExecuteNonQuery()
+        lector = command.ExecuteReader()
+
         While lector.Read()
             DataGridView1.Rows.Add(lector(0), lector(1), lector(2), lector(3), lector(4), lector(5), lector(6), lector(7))
         End While
